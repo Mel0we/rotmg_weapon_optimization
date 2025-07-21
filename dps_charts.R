da268fd4e51c5a6a7b32a851fa374abf4559d7cd
Mel0we <melowe2427@gmail.com>
2025-07-21 10:32:37 -0400

    Update dps_charts.R
    Progress made on custom function to add class/weapon dps columns to dps_matrix. Still needs to rename column as it's created.

--- dps_charts.R
+++ dps_charts.R
@@ -18,29 +18,24 @@ def_list <- 0:60
 dps_matrix <- def_list
 
 # Assign index of desired class/weapon for dps calculation
+# Can also make lists for multiple comparisons
 class_index <- 12
-weapon_index <- 13
+weapon_index <- 4
 
 # Attempt to build function for weapon/class dps
-# Not working right now, run as separate lines
+# Updates dps_matrix based on class/weapon
+# Still updating functionality to rename dps column
+# as it is added
 
-# dps_calc <- function(class_index, weapon_index) {
-  # average damage taken from dagger table at assigned index
-  avg_dmg <- dagger_table$average_damage[weapon_index]
-  # dmg_mult based on class atk stat and atk formula
-  dmg_mult <- 0.5 + stats_table$ATT[class_index] / 50
-  # num_shots will generally be 1 but can be higher
-  # Each bullet affected by def individually
-  num_shots <- dagger_table$num_shots[weapon_index] 
-  # atks_per_sec based on class dex stat and attack speed formula
-  atks_per_sec <- 1.5 + 6.5 * (stats_table$DEX[class_index] / 75)
-  # Creates a vector of dps values based on the DEF list
+dps_calc <- function(class, weapon) {
+  avg_dmg <- dagger_table$average_damage[weapon]
+  dmg_mult <- 0.5 + stats_table$ATT[class] / 50
+  num_shots <- dagger_table$num_shots[weapon]
+  atks_per_sec <- 1.5 + 6.5 * (stats_table$DEX[class] / 75)
   dps <- (avg_dmg * dmg_mult - def_list) * num_shots * atks_per_sec
-# }
-
-# Append dps from desired class/weapon combo onto dps_matrix
-## Currently trying to append while renaming dps vector to weapon name
-dps_matrix <- cbind(dps_matrix, dagger_table$Name[weapon_index] <- dps)
+  dps_matrix <<- cbind(dps_matrix, dps)
+}
+dps_calc(class_index, weapon_index)
 
 # Finally we can visualize the relationship
 dps_matrix |> ggplot(
