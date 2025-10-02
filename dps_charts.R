c6697cb238e54c950a850afde70115b6a92420ae
Mel0we <melowe2427@gmail.com>
2025-10-02 14:51:27 -0400

    Update dps_charts.R

--- dps_charts.R
+++ dps_charts.R
@@ -26,8 +26,6 @@ weapon_index <- 13
 # Attempt to build function for weapon/class dps
 # Appends dps column to dps matrix
 # and renames based on weapon name
-## Rename functionality is not working yet
-## Will come back to fix at some point
 
 dps_calc <- function(class, weapon) {
   avg_dmg <- dagger_table$average_damage[weapon]
@@ -35,26 +33,46 @@ dps_calc <- function(class, weapon) {
   num_shots <- dagger_table$num_shots[weapon]
   atks_per_sec <- 1.5 + 6.5 * (stats_table$DEX[class] / 75)
   dps <- (avg_dmg * dmg_mult - def_list) * num_shots * atks_per_sec
-  dps_matrix <<- cbind(dps_matrix, dps)
-  # weapon_name <- 'w'
-  # assign(weapon_name, dagger_table$Name[weapon])
-  # rename(dps_matrix, weapon_name = dps)
+  
+  weapon_name <- dagger_table$Name[weapon]
+  new_col <- setNames(data.frame(dps), weapon_name)
+  dps_matrix <<- cbind(dps_matrix, new_col)
 }
-dps_calc(12, 13)
-
 
 # Insert desired class/weapon combos to compare in comp_list
 comp_list <- list(c(12, 13), c(12, 14), c(12, 15))
 # The following loop applies those combos to the dps_calc function
-## ^This does not work like desired
 for (i in comp_list) {
-  dps_calc(comp_list)
+  dps_calc(i[1], i[2])
 }
 
+# Need to pivot to long data for visualization
+dps_long <- pivot_longer(
+  dps_matrix, 
+  cols = -DEF,
+  names_to = 'Weapon',
+  values_to = 'DPS'
+)
 
 # Finally we can visualize the relationship
-dps_matrix |> ggplot(
-  aes(x = DEF, y = dps)) +
-  geom_line(color = 'red') +
-  labs(x = 'Enemy Defence', y = 'Damage per Second (DPS)') +
+# Would like to reorder legend by order in dps_long, not alphabetical
+dps_long |> ggplot(
+  aes(x = DEF, y = DPS, color = Weapon)) +
+  geom_line() +
+  scale_x_continuous(breaks = seq(0,60, by = 10)) +
+  scale_color_manual(
+    values = c(
+      'Dagger_of_Foul_Malevolence' = '#CB2929',
+      'Dagger_of_Sinister_Deeds' = '#E97812',
+      'Dagger_of_Dire_Hatred' = '#3750FB'
+      ), 
+    breaks = c(
+      'Dagger_of_Dire_Hatred', 
+      'Dagger_of_Sinister_Deeds', 
+      'Dagger_of_Foul_Malevolence'
+      )
+  ) +
+  labs(x = 'Enemy Defence', 
+       y = 'Damage per Second (DPS)',
+       title = 'Comparison of Trickster DPS w/ Tiered Daggers') +
   theme_minimal()
